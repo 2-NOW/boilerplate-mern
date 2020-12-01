e68be7bf51264c437380aeed50a65bac43d52c94
Gappleg <kj109888@gmail.com>
2020-12-01 16:33:21 +0900

    boilerplate-mern

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
 const config = require("./config/key");
-
+ 
 // const mongoose = require("mongoose");
 // mongoose
 //   .connect(config.mongoURI, { useNewUrlParser: true })
